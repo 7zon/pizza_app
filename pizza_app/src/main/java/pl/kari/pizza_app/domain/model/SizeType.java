94467fecb555f9d958b6d2235dc78d66ff1cdf34
Kariku <karina.drobek@tm1.edu.pl>
2021-11-26 13:39:11 +0100

    initialpush

---
+++ pizza_app/src/main/java/pl/kari/pizza_app/domain/model/SizeType.java
@@ -0,0 +1,7 @@
+package pl.kari.pizza_app.domain.model;
+
+public enum SizeType {
+
+    S, M, L
+
+}
---
+++ pizza_app/src/test/java/pl/kari/pizza_app/PizzaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kari.pizza_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
